c04d44f6b51f3e11e87d5bcdfd77bfb014724c6a
Natalia Merino <nati.merino@gmail.com>
2013-10-28 23:31:05 -0300

    probando commit en repo externo

--- FrameworkTests/src/main/java/Grupo11/FrameworkTests/App.java
+++ FrameworkTests/src/main/java/Grupo11/FrameworkTests/App.java
@@ -4,6 +4,9 @@ package Grupo11.FrameworkTests;
  * Hello world!
  *
  */
+
+/* NatiMerino: probando commit con git en repo externo*/
+
 public class App 
 {
     public static void main( String[] args )
