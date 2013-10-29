1c927e6e1df05911c5b8ad2f508e9389eec7bef5
pellele13 <lrpellegrino@gmail.com>
2013-10-28 23:41:46 -0300

    Modelo de Dominio y estructura general del proyecto

--- FrameworkTests/src/main/java/Grupo11/FrameworkTests/App.java
+++ FrameworkTests/src/main/java/Grupo11/FrameworkTests/App.java
@@ -1,16 +1,9 @@
 package Grupo11.FrameworkTests;
 
-/**
- * Hello world!
- *
- */
-
-/* NatiMerino: probando commit con git en repo externo*/
-
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Ya tengo el poder!" );
     }
 }
