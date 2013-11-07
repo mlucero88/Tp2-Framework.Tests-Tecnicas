df5420984ff0f0b28793794a9afc9ff3ea4b991a
Natalia Merino <nati.merino@gmail.com>
2013-11-07 00:02:37 -0300

    Corrección de los formatos de los mensajes para que coincidan con el formato pedido para el reporte

--- FrameworkTests/src/main/java/grupo11/frameworktests/TestResult.java
+++ FrameworkTests/src/main/java/grupo11/frameworktests/TestResult.java
@@ -3,18 +3,21 @@ package grupo11.frameworktests;
 /* Clase que almacena el resultado de un UnitTest */
 
 class TestResult {
-	private final static String MESSAGE_SUCCESSFUL = "Succeeded";
-	private final static String MESSAGE_FAILURE = "Failed";
+	private final static String MESSAGE_SUCCESSFUL = "OK";
+	private final static String MESSAGE_FAILURE = "Fail";
+	private final static String MESSAGE_ERROR = "Error";
 	private String result;
 
 	static TestResult createSuccessfulResult(String testName) {
-		return new TestResult("TEST: " + testName + " - RESULT: "
-				+ MESSAGE_SUCCESSFUL);
+		return new TestResult("[" + MESSAGE_SUCCESSFUL + "] " + testName);
 	}
 
-	static TestResult createFailedResult(String testName, String errorMessage) {
-		return new TestResult("TEST: " + testName + " - RESULT: "
-				+ MESSAGE_FAILURE + " - REASON: " + errorMessage);
+	static TestResult createFailedResult(String testName, String failureMessage) {
+		return new TestResult("[" + MESSAGE_FAILURE + "] " + testName);
+	}
+
+	static TestResult createErrorResult(String testName, String errorMessage) {
+		return new TestResult("[" + MESSAGE_ERROR + "] " + testName);
 	}
 
 	public String getMessage() {
