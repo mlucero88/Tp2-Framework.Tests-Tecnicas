abcef8bbabd1c61bc9139498258126a54294a8a3
Hugo Chavar <hechavar@gmail.com>
2013-11-19 13:55:29 -0300

    Refactor de UnitTest para poder levantar xml

--- FrameworkTests/src/main/java/grupo11/frameworktests/UnitTest.java
+++ FrameworkTests/src/main/java/grupo11/frameworktests/UnitTest.java
@@ -1,22 +1,25 @@
 package grupo11.frameworktests;
 
+import org.jdom.Attribute;
+import org.jdom.Element;
+
 /* Clase abstracta de la cual el cliente hereda para crear su test unitario. El
  * cliente debe implementar el metodo "test" con el comportamiento que desea
  * testear */
 
 public abstract class UnitTest extends GenericTest {
 
-	// MOD0568 - BEGIN
 	public enum ResultType {
 		Ok, Fail, Error
 	}
 
 	private ResultType resultType;
+	double timeTotal;
+	String errorMsg = new String("");
 
 	public boolean isOK() {
-		return (resultType == ResultType.Ok) ? true : false;
+		return (resultType == ResultType.Ok);
 	}
-	// MOD0568 - END
 	
 	public UnitTest(String testName) {
 		super(testName);
@@ -41,7 +44,6 @@ public abstract class UnitTest extends GenericTest {
 	@Override
 	final public UnitTestResult run() {
 		UnitTestResult result;
-		double timeTotal;
 		double timeStart = System.currentTimeMillis();
 		try {
 			setUp();
@@ -53,18 +55,20 @@ public abstract class UnitTest extends GenericTest {
 		}
 		catch (ValidationFailure failure) {
 			timeTotal = (System.currentTimeMillis() - timeStart);
+			errorMsg = failure.getMessage();
 			result =
 					UnitTestResult.createFailedResult(getName(),
-							failure.getMessage());
+							errorMsg);
 			result.setTiempoEjecucion(timeTotal);
 			System.out.println(result.getMessage());
 			return result;
 		}
 		catch (RuntimeException exception) {
 			timeTotal = (System.currentTimeMillis() - timeStart);
+			errorMsg = "RuntimeException";
 			result =
 					UnitTestResult.createErrorResult(getName(),
-							"RuntimeException");
+							errorMsg);
 			result.setTiempoEjecucion(timeTotal);
 			System.out.println(result.getMessage());
 			return result;
@@ -82,5 +86,33 @@ public abstract class UnitTest extends GenericTest {
 
 	public String getTestCollectionContenedora() {
 		return null;
+	}
+	
+	@Override
+	public Element toXMLElement() {
+		Element element = new Element("testcase");
+		element.setAttribute("name", getName());
+		element.setAttribute("status", resultType.name());
+		element.setAttribute("time", String.valueOf(timeTotal));
+		if (resultType != ResultType.Ok) {
+			element.setAttribute(new Attribute("message", errorMsg));
+		}
+
+		return element;
+	}
+	
+	@Override
+	public Integer countTests() {
+		return 1;
+	}
+
+	@Override
+	public Integer countErrors() {
+		return (resultType == ResultType.Error) ? 1 : 0;
+	}
+
+	@Override
+	public Integer countFailures() {
+		return (resultType == ResultType.Fail) ? 1 : 0;
 	}
 }
